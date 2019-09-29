cd4072f835e2ad26ea18259f403ebd7aea11021d
novakvova <novakvova@gmail.com>
2019-09-29 10:56:37 +0300

    Hello world

--- TestSalutSemen/Program.cs
+++ TestSalutSemen/Program.cs
@@ -10,6 +10,8 @@ namespace TestSalutSemen
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
